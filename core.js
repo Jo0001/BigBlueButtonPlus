0bf805fdf4f54cc098f7342356742f340813fd2f
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2021-01-19 14:40:09 +0100

    Initial commit

---
+++ core.js
@@ -0,0 +1,44 @@
+let raise = false;
+let btn;
+const defaultId = 8;
+
+setTimeout(function () {
+    load();
+}, 3000);
+
+function load() {
+    console.info("Loading BigBlueButton+");
+    document.title = "BigBlueButton+ Videokonferenz";
+    btn = document.createElement("button");
+    btn.innerHTML = '<svg height="24" width="24" id="hand" viewBox="0 0 32 32" style="margin-left: -3px"><path d="M30.688 7.313v19.375c0 2.938-2.438 5.313-5.375 5.313h-9.688a5.391 5.391 0 01-3.813-1.563L1.312 19.75S3 18.125 3.062 18.125a1.7 1.7 0 011.063-.375c.313 0 .563.063.813.188.063 0 5.75 3.25 5.75 3.25V5.313c0-1.125.875-2 2-2s2 .875 2 2v9.375h1.313V2c0-1.125.875-2 2-2s2 .875 2 2v12.688h1.313V3.313c0-1.125.875-2 2-2s2 .875 2 2v11.375h1.375V7.313c0-1.125.875-2 2-2s2 .875 2 2z"></path></svg>';
+    btn.style = "background: white; cursor: pointer;border: 2px solid white;border-radius: 50px;";
+    btn.onclick = toggle;
+    btn.title = "Hand heben";
+    document.getElementById("tippy-19").parentElement.append(btn);
+}
+
+function toggle() {
+    let hand = document.getElementById("hand");
+    if (!raise) {
+        //raise hand
+        document.getElementsByClassName("item--yl1AH")[6].click();
+        document.getElementsByClassName("item--yl1AH")[getItem()].click();
+        btn.title = "Hand herunternehmen";
+        hand.style.fill = "#0F70D7";
+    } else {
+        //clear
+        document.getElementsByClassName("item--yl1AH")[7].click();
+        btn.title = "Hand heben";
+        hand.style.fill = "black";
+    }
+    raise = !raise;
+}
+
+function getItem() {
+    let fromLocal = localStorage.getItem("bbb_plus_id");
+    if (fromLocal !== null && !isNaN(parseInt(fromLocal))) {
+        return parseInt(fromLocal);
+    } else {
+        return defaultId;
+    }
+}
