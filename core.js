97d98f967c3249cf530215c3c5d61d5acd8e1b9a
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2021-01-24 19:29:01 +0100

    added mod check & fixed unmute toggle

--- core.js
+++ core.js
@@ -1,14 +1,19 @@
 let raise = false;
+let addedUnmuteListener = false;
 let btn, baseElement, hand, delay;
 const defaultId = 8;
 
 setTimeout(function () {
-    load();
-}, 6000);//6sec
-
-function load() {
     console.info("Loading BigBlueButton+");
     document.title = "BigBlueButton+ Videokonferenz";
+    if (!isMod()) loadHandRaise();
+}, 6000);//6sec
+
+function isMod() {
+    return document.querySelectorAll('[aria-label~="You"]')[0].parentElement.children[0].children[0].classList.contains("moderator--24bqCT");
+}
+
+function loadHandRaise() {
     btn = document.createElement("button");
     btn.innerHTML = '<svg height="24" width="24" id="hand" viewBox="0 0 32 32" style="margin-left: -3px"><path d="M30.688 7.313v19.375c0 2.938-2.438 5.313-5.375 5.313h-9.688a5.391 5.391 0 01-3.813-1.563L1.312 19.75S3 18.125 3.062 18.125a1.7 1.7 0 011.063-.375c.313 0 .563.063.813.188.063 0 5.75 3.25 5.75 3.25V5.313c0-1.125.875-2 2-2s2 .875 2 2v9.375h1.313V2c0-1.125.875-2 2-2s2 .875 2 2v12.688h1.313V3.313c0-1.125.875-2 2-2s2 .875 2 2v11.375h1.375V7.313c0-1.125.875-2 2-2s2 .875 2 2z"></path></svg>';
     btn.style = "background: white; cursor: pointer;border: 2px solid white;border-radius: 50px;";
@@ -17,9 +22,6 @@ function load() {
     baseElement = document.getElementsByClassName("buttonWrapper--x8uow button--295UAi")[0];
     baseElement.parentElement.append(btn);
     hand = document.getElementById("hand");
-    setTimeout(function () {
-        baseElement.addEventListener('click', unmuteClear);
-    }, 30000);//30sec
 }
 
 function startAutoTimeout() {
@@ -46,6 +48,10 @@ function toggle() {
         hand.style.fill = "#0F70D7";
         raise = true;
         startAutoTimeout();
+        if (!addedUnmuteListener) {
+            baseElement.addEventListener('click', unmuteClear);
+            addedUnmuteListener = true;
+        }
     } else {
         lowerHand();
         clearTimeout(delay);
