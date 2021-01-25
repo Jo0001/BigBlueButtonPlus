83b9de493ddc892a3befbe508b3ac00275ae3c53
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2021-01-25 19:19:41 +0100

    fixed isMod

--- core.js
+++ core.js
@@ -4,32 +4,50 @@ let btn, baseElement, hand, delay;
 const defaultId = 8;
 
 setTimeout(function () {
-    console.info("Loading BigBlueButton+");
+    console.info("[BBB+] Loading BigBlueButton+");
     document.title = "BigBlueButton+ Videokonferenz";
     if (!isMod()) loadHandRaise();
-}, 6000);//6sec
+}, 30000);//30sec
 
 function isMod() {
-    return document.querySelectorAll('[aria-label~="You"]')[0].parentElement.children[0].children[0].classList.contains("moderator--24bqCT");
+    let mod = false;
+    try {
+        try {
+            if (document.querySelectorAll('[aria-label~="You"]')[0].parentElement.children[0].children[0].classList.contains("moderator--24bqCT")) {
+                mod = true;
+            }
+        } catch (e) {
+            if (document.querySelectorAll('[aria-label~="Sie"]')[0].parentElement.children[0].children[0].classList.contains("moderator--24bqCT")) {
+                mod = true;
+            }
+        }
+    } catch (e) {
+        console.warn("[BBB+] Error on detecting moderator privileges" + e);
+    }
+    return mod
 }
 
 function loadHandRaise() {
-    btn = document.createElement("button");
-    btn.innerHTML = '<svg height="24" width="24" id="hand" viewBox="0 0 32 32" style="margin-left: -3px"><path d="M30.688 7.313v19.375c0 2.938-2.438 5.313-5.375 5.313h-9.688a5.391 5.391 0 01-3.813-1.563L1.312 19.75S3 18.125 3.062 18.125a1.7 1.7 0 011.063-.375c.313 0 .563.063.813.188.063 0 5.75 3.25 5.75 3.25V5.313c0-1.125.875-2 2-2s2 .875 2 2v9.375h1.313V2c0-1.125.875-2 2-2s2 .875 2 2v12.688h1.313V3.313c0-1.125.875-2 2-2s2 .875 2 2v11.375h1.375V7.313c0-1.125.875-2 2-2s2 .875 2 2z"></path></svg>';
-    btn.style = "background: white; cursor: pointer;border: 2px solid white;border-radius: 50px;";
-    btn.onclick = toggle;
-    btn.title = "Hand heben";
-    baseElement = document.getElementsByClassName("buttonWrapper--x8uow button--295UAi")[0];
-    baseElement.parentElement.append(btn);
-    hand = document.getElementById("hand");
+    try {
+        btn = document.createElement("button");
+        btn.innerHTML = '<svg height="24" width="24" id="hand" viewBox="0 0 32 32" style="margin-left: -3px"><path d="M30.688 7.313v19.375c0 2.938-2.438 5.313-5.375 5.313h-9.688a5.391 5.391 0 01-3.813-1.563L1.312 19.75S3 18.125 3.062 18.125a1.7 1.7 0 011.063-.375c.313 0 .563.063.813.188.063 0 5.75 3.25 5.75 3.25V5.313c0-1.125.875-2 2-2s2 .875 2 2v9.375h1.313V2c0-1.125.875-2 2-2s2 .875 2 2v12.688h1.313V3.313c0-1.125.875-2 2-2s2 .875 2 2v11.375h1.375V7.313c0-1.125.875-2 2-2s2 .875 2 2z"></path></svg>';
+        btn.style = "background: white; cursor: pointer;border: 2px solid white;border-radius: 50px;";
+        btn.onclick = toggle;
+        btn.title = "Hand heben";
+        baseElement = document.getElementsByClassName("buttonWrapper--x8uow button--295UAi")[0];
+        baseElement.parentElement.append(btn);
+        hand = document.getElementById("hand");
+    } catch (e) {
+        console.error("[BBB+] Error on loadHandRaise() " + e);
+    }
 }
 
 function startAutoTimeout() {
     delay = setTimeout(function () {
-        if (confirm("Du streckst seit 3min \nHand herunternehmen?")) {
+        if (confirm("Du streckst seit 2,5min \nHand herunternehmen?")) {
             lowerHand();
         }
-    }, 180000);//3min
+    }, 150000);//2,5min
 }
 
 function unmuteClear() {
